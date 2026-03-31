8d35828f5b5630b77814b2ffce79e92f9abf2452
Roony-6 <roony7.rrc@gmail.com>
2026-03-31 13:31:35 -0600

    Add main application structure and initial content for numerical methods project

---
+++ app.py
@@ -0,0 +1,42 @@
+import streamlit as st
+
+# 1. Configuración (Debe ser la primera línea de Streamlit)
+st.set_page_config(
+    page_title="Proyecto Metodos Numericos",
+    page_icon="R",
+    layout="wide"
+)
+
+# 2. Título y Bienvenida
+st.title("Proyecto Final Metodos Numericos")
+st.subheader("Calculadora de raices")
+
+st.markdown("---")
+
+# 3. Información del Estudiante (Presentación)
+with st.sidebar:
+    st.info(" **Roony Roldan Cruz** \n\n **Institucion:** Instituto Politecnico Nacional \n\n **Universidad:** Escuela Superior de Computo")
+
+# 4. Contenido Principal
+col1, col2 = st.columns([2, 1])
+
+with col1:
+    st.write("""
+    ### Calculadora de raices 
+    Esta aplicación permite resolver problemas complejos para encontrar raices con 
+    la implementación de algoritmos matemáticos **.
+    
+    **Secciones disponibles:**
+    * **Raíces de Ecuaciones:** Bisección, Newton-Raphson, Secante,etc.
+    * **Interpolación:** Lagrange, Newton (Diferencias Divididas).
+    * **Integración:** Regla del Trapecio y Simpson.
+    """)
+    
+    # Ejemplo de fórmula en LaTeX
+    #st.latex(r"f(x) = \sum_{i=0}^{n} y_i L_i(x)")
+
+#with col2:
+    # Puedes poner una imagen o un logo aquí
+    #st.image("https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1200px-Python-logo-notext.svg.png", width=150)
+
+st.success(" <- Selecciona un método en el menú de la izquierda para comenzar.")
